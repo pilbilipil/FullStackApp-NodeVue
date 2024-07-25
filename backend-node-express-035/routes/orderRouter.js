93ae8782dec09410ad5acc32e8e4b2c24112524b
Shungus <59698213+pilbilipil@users.noreply.github.com>
2024-07-25 17:56:21 +0500

    Delete backend-node-express-035 directory

--- backend-node-express-035/routes/orderRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderControler = require('../controller/orderControler');
-
-/// get all Order API
-router.get('/', orderControler.getOrder);
-
-module.exports = router;
