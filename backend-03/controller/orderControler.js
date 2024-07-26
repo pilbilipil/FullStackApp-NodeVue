11ea9e3199692efb557927f250f703f0f18ea673
Shungus <59698213+pilbilipil@users.noreply.github.com>
2024-07-26 18:19:00 +0500

    Add files via upload

---
+++ backend-03/controller/orderControler.js
@@ -0,0 +1,67 @@
+const orderModel = require('../models/orderModel');
+
+exports.getOrder = async(res) =>
+{
+  try 
+  {
+  const getOrderDate = await orderModel.getAllOrder();
+  res.json(getOrderDate);
+  } 
+  catch(error) 
+  {
+  res.status(500).json({error: 'Database error'});
+  };
+}; 
+
+exports.getOrderById = async(req, res) => 
+{
+  try 
+  {
+  const getOrderById = await orderModel.getOrderById(req);
+  res.json(getOrderById);
+  }
+  catch(error) 
+  {
+  res.status(500).json({error: 'Database error'});
+  }
+}
+
+exports.updateOrderById = async(req,res) => 
+{
+  try
+  {
+  const updatedOrder = await orderModel.updateOrder(req);
+  res.json(updatedOrder);    
+  }
+  catch(error) 
+  {
+  res.status(500).json({error: 'Database error'});    
+  }
+}
+
+exports.createOrder = async(req,res) => 
+{
+  try 
+  {
+  const createdOrder = await orderModel.createOrderByItem(req);
+  res.json(createdOrder);  
+  } 
+  catch(error) 
+  {
+  res.status(500).json({error: 'Database error'});
+  }
+}
+
+exports.deleteOrderById = async(req,res) => 
+{
+  try 
+  {
+  const deletedOrder = await orderModel.deleteOrder(req);
+  res.json(deletedOrder);
+  } 
+  catch(error) 
+  {
+  res.status(500).json({error: 'Database error'});
+  }
+}
+
