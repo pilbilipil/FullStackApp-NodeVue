ea73a5af3c958484b48fa1188a90bdeb45843eb6
Shungus <59698213+pilbilipil@users.noreply.github.com>
2024-07-29 18:58:02 +0500

    Add files via upload

---
+++ backend-05/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port =  process.env.BACKEND_PORT;
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/userRouter.js');
+const orderRouter = require('./routes/orderRouter.js');
+const productRouter = require('./routes/productRouter.js');
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/order', orderRouter);
+app.use('/api/v1/product', productRouter);
+
+app.listen(port, () => {
+    console.log("server running on port " + port);
+})
---
+++ backend-05/routes/productRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const productControler = require('../controller/productControler');
+
+/// get all Product API
+router.get('/', productControler.getProduct);
+
+/// create new Product API
+router.post('/', productControler.createProduct);
+
+/// get,put,delete by id APIs
+router.get('/:id', productControler.getProductById);
+router.delete('/:id', productControler.deleteProductById);
+router.put('/:id', productControler.updateProduct);
+
+module.exports = router;
