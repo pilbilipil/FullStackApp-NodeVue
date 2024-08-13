5b140fe599fc0981274faab29177ae40b4b35e37
Shungus <59698213+pilbilipil@users.noreply.github.com>
2024-08-13 18:50:09 +0500

    Delete backend-11 directory

--- backend-11/src/config/corsConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({
-    origin: process.env.CORS_ORIGIN, 
-    methods: process.env.CORS_METHODS, 
-    allowedHeaders: process.env.CORS_HEADERS, 
-    credentials: process.env.CORS_CREDENTIALS 
-})
